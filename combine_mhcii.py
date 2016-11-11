e06cb2bb73a2f7dee7b6c827b2c7648961550e19
sinkovit <sinkovit@sdsc.edu>
2016-11-10 18:12:32 -0800

    Moved some functionality out of parse_mhcii.py into mhcii.py. Created new script, combine_mhcii.py for annotating foreign peptide prediction results with TCR class occupancy for self peptides

---
+++ combine_mhcii.py
@@ -0,0 +1,92 @@
+# Program combine_mhcii.py
+#
+# Description: Combine the results from a pair of MHC Class II peptide
+# binding prediction analyses.
+#
+# Input:
+# (1) Results of MHC class II prediction for foreign proteome
+# (2) Results of MHC class II prediction for self proteome
+# (3) IC50 cutoff
+# (4) Method (NN or SMM)
+#
+# Output:
+#
+# (1) MHC class II binding prediction results for foreign proteome
+# annotated by TCR class and occupancy from reference proteome. The
+# method and IC50 cutoff are used both to filter the results from the
+# foreign proteome and to determine the class occupancy for the
+# reference proteome. Note that both self and foreign predictions need
+# to be based on same MHC II molecule
+
+import argparse
+import mhcii
+
+parser = argparse.ArgumentParser(description='Annotate results of foreign MHC II peptide binding prediction with TCR occupancies from self binding predictions. Done using a specified method and IC50 cutoff')
+
+parser.add_argument('-s', dest='self_prediction',
+                    help='MHC II predictions for self peptides')
+parser.add_argument('-f', dest='foreign_prediction',
+                    help='MHC II predictions for foreign peptides')
+parser.add_argument('-r', dest='results',
+                    help='MHC II predictions for foreign peptides')
+parser.add_argument('-m', dest='method', default='nn',
+                    help='MHC II prediction method (nn or smm)')
+parser.add_argument('-c', dest='cutoff', type=int,
+                    help='Cutoff affinity (IC50) in nM')
+
+args                = parser.parse_args()
+self_prediction     = args.self_prediction
+foreign_prediction  = args.foreign_prediction
+results             = args.results
+method              = args.method
+cutoff              = args.cutoff
+
+print 'Self proteome prediction file:    ', self_prediction
+print 'Foreign proteome prediction file: ', foreign_prediction
+print 'MHC binding prediction method:    ', method
+print 'IC50 cuotff:                      ', cutoff
+
+# Determine TCR class occupancies for self peptides
+occupancy = {}
+if method == 'smm':
+    nn_file, smm_file = mhcii.mhcii_to_TCR_classes(self_prediction, 0, cutoff)
+    self_occ = smm_file
+else:
+    nn_file, smm_file = mhcii.mhcii_to_TCR_classes(self_prediction, cutoff, 0)
+    self_occ = nn_file
+
+with open(self_occ, 'r') as fin:
+    for line in fin:
+        tcr_class, tcr_occ = line.split()
+        occupancy[tcr_class] = tcr_occ
+
+
+# Process the foreign peptide binding file and write annotated results
+fout = open(results, 'w')
+with open(foreign_prediction, 'r') as fin:
+    next(fin)
+    for line in fin:
+        fields = line.split()
+
+        # Get information on peptide, SMM scoring and NN scoring
+        peptide  = fields[4]
+        smm_core = fields[10]
+        smm_ic50 = float(fields[11])
+        smm_rank = float(fields[12])
+        nn_core  = fields[13]
+        nn_ic50  = float(fields[14])
+        nn_rank  = float(fields[15])
+
+        # Determine the TCR class from core nonamer positions 2,3,5 and 8
+        # Note that code reflects Python counting from zero
+        tcr_smm_class = smm_core[1]+smm_core[2]+smm_core[4]+smm_core[7]
+        tcr_nn_class  = nn_core[1]+nn_core[2]+nn_core[4]+nn_core[7]
+
+        if method == 'smm' and smm_ic50 < cutoff:
+            s = peptide + " " + smm_core + " " + tcr_smm_class + " " + str(smm_ic50) + " " + occupancy[tcr_smm_class] + "\n"
+            fout.write(s)
+        if method == 'nn' and nn_ic50 < cutoff:
+            s = peptide + " " +  nn_core + " " + tcr_nn_class + " "  + str(nn_ic50)  + " " + occupancy[tcr_nn_class]  + "\n"
+            fout.write(s)
+
+fout.close()
--- parse_mhcii.py
+++ parse_mhcii.py
@@ -10,9 +10,9 @@
 # Author: Robert Sinkovits, San Diego Supercomputer Center
 
 import argparse
+import mhcii
 
 parser = argparse.ArgumentParser(description='Parse MHC II prediction data and sort peptides into T cell receptor classes')
-
 parser.add_argument(dest='infile',
                     help='Input file in FASTA format')
 parser.add_argument('-n', dest='nn_cutoff', default=0, type=int,
@@ -25,92 +25,4 @@ infile        = args.infile
 nn_cutoff     = args.nn_cutoff
 smm_cutoff    = args.smm_cutoff
 
-# Initialize TCR classes
-smm_dict = {}
-nn_dict  = {}
-aas = 'ACDEFGHIKLMNPQRSTVWY'
-for pos1 in aas:
-    for pos2 in aas:
-        for pos3 in aas:
-            for pos4 in aas:
-                tcr_class = pos1+pos2+pos3+pos4
-                smm_dict[tcr_class] = 0
-                nn_dict[tcr_class]  = 0
-
-
-# Initialize counts for peptides with bindings below affinity
-# threshold, total number of peptides and number of empty classes
-
-nn_count_below_IC50  = 0
-smm_count_below_IC50 = 0
-smm_empty_classes    = 0
-nn_empty_classes     = 0
-peptide_count        = 0
-
-# Open and parse output from MHC II prediction program
-with open(infile, 'rU') as fin:
-    next(fin) # Skipping header line
-    for line in fin:
-        peptide_count += 1
-        fields = line.split()
-
-        # Get information on peptide, SMM scoring and NN scoring
-        peptide  = fields[4]
-        smm_core = fields[10]
-        smm_ic50 = float(fields[11])
-        smm_rank = float(fields[12])
-        nn_core  = fields[13]
-        nn_ic50  = float(fields[14])
-        nn_rank  = float(fields[15])
-
-        # Determine the TCR class from core nonamer positions 2,3,5 and 8
-        # Note that code reflects Python counting from zero
-        tcr_smm_class = smm_core[1]+smm_core[2]+smm_core[4]+smm_core[7]
-        tcr_nn_class  = nn_core[1]+nn_core[2]+nn_core[4]+nn_core[7]
-
-        if smm_ic50 < smm_cutoff:
-            smm_dict[tcr_smm_class] += 1
-            smm_count_below_IC50 += 1
-        if nn_ic50 < nn_cutoff:
-            nn_count_below_IC50 += 1
-            nn_dict[tcr_nn_class]   += 1
-
-# Write results for SMM method
-if smm_cutoff > 0:
-    sorted_keys = sorted(smm_dict.keys())
-    smm_empty_classes = 0
-    fout = open('smm_'+str(smm_cutoff)+'.out', 'w')
-    for k in sorted_keys:
-        if smm_dict[k] == 0:
-            smm_empty_classes += 1
-        s = k + " " + str(smm_dict[k]) + "\n"
-        fout.write(s)
-    fout.close()
-
-    pct_binding = 100 * float(smm_count_below_IC50)/float(peptide_count)
-    print
-    print 'SMM statistics'
-    print 'IC50 cutoff:              ', smm_cutoff
-    print '# peptides:               ', peptide_count
-    print '# peptides IC50 < cutoff: ', smm_count_below_IC50
-    print 'Percent binding:          ', pct_binding
-
-# Write results for NN method
-if nn_cutoff > 0:
-    sorted_keys = sorted(nn_dict.keys())
-    nn_empty_classes = 0
-    fout = open('nn_'+str(nn_cutoff)+'.out', 'w')
-    for k in sorted_keys:
-        if nn_dict[k] == 0:
-            nn_empty_classes += 1
-        s = k + " " + str(nn_dict[k]) + "\n"
-        fout.write(s)
-    fout.close()
-
-    pct_binding = 100 * float(nn_count_below_IC50)/float(peptide_count)
-    print
-    print 'NN statistics'
-    print 'IC50 cutoff:              ', nn_cutoff
-    print '# peptides:               ', peptide_count
-    print '# peptides IC50 < cutoff: ', nn_count_below_IC50
-    print 'Percent binding:          ', pct_binding
+mhcii.mhcii_to_TCR_classes(infile, nn_cutoff, smm_cutoff)
